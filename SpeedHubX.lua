d82952425db658fc11d1b7dbd1027f798a846e5a
luh <114599798+luh-99@users.noreply.github.com>
2025-05-08 07:51:29 -0500

    Create SpeedHubX.lua

---
+++ SpeedHubX.lua
@@ -0,0 +1,27 @@
+-- Working -> 🟢
+
+-- # Update -> 🟠
+
+-- Not Working -> 🔴
+
+-- Discontinued -> ⚫
+
+-----< (Game) >-----
+
+-- Blox Fruit : 🟢
+-- Muscle Legends : 🟢
+-- Pet simulator 99 : ⚫
+-- Gym League : ⚫
+-- The Strongest Battlegrounds : 🟢
+-- Doors : ⚫
+-- Anime Vanguards : 🟢
+-- Pet GO : 🟢
+-- Fisch : 🟢
+-- Anime Reborn : ⚫
+-- Jujutse Infinite : ⚫
+-- Dead Rails : 🟢
+--Arise Crossover : 🟢
+-- Bubble Gum Simulator INFINITY : 🟢
+-- Grow a Garden : 🟢
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
